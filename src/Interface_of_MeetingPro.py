d435223a5e6dec36685d2abc5437b6fea3529397
Bismuth <dorianbismuth@gmail.com>
2025-05-30 10:15:36 +0200

    interface 1.1, ameliorer mais sans la taille des boutons

--- src/Interface_of_MeetingPro.py
+++ src/Interface_of_MeetingPro.py
@@ -10,13 +10,27 @@ def create_interface():
     
     # Create a label
     label = tk.Label(root, text="Welcome to the Interface!")
-    label.pack(pady=90)
+    label.pack(pady=50)
+    label.config(font=("Arial", 24))  # Set the font size and type
+    label.size = (300, 20)  # Set the size of the label
     
-    # Create a button
-    button = tk.Button(root, text="Add a new customer", command=lambda: print("Button clicked!"))
-    button.pack(pady=10)
+
+
+    # Create a button to add a new customer
+    button_new_customer = tk.Button(root, text="Add a new customer", command=lambda: print("Button clicked!"), bg="blue", fg="white")
+    button_new_customer.pack(pady=30)
+
+    # Create a button to add a new reservation
+    button_reservation = tk.Button(root, text="Add a new reservation", command=lambda: print("Reservation button clicked!"), bg="blue", fg="white")
+    button_reservation.pack(pady=30)
+
+    # Create a button to add to display informations
+    button_display = tk.Button(root, text="Display Information",command=lambda: print("Display Information button clicked!"), bg="blue", fg="white")
+    button_display.pack(pady=30)
+
+
     
     # Start the GUI event loop
     root.mainloop()
 
-create_interface()
+create_interface() 
